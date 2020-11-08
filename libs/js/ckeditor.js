32138f215ab6bca7925ddce239b0b8e7fc3ad0bb
teachers-uk-net <postmaster@teachers.uk.net>
2020-11-08 22:43:55 +0000

    Started conversion to OOP using PDO

---
+++ libs/js/ckeditor.js
@@ -0,0 +1,17 @@
+var allEditors = document.querySelectorAll('.editor');
+for (var i = 0; i < allEditors.length; ++i){
+    ClassicEditor
+        .create(allEditors[i],{
+            toolbar: [ 'heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList' ],
+            heading: {
+                options: [
+                    {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+                    {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+                    {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'}
+                ]
+            }
+        } )
+        .catch( error => {
+            console.error( error );
+        } );
+}
