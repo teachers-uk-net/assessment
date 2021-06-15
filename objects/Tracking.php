50071b21ae48bc05e31e3be580d63e9cfec96bbb
teachers-uk-net <postmaster@teachers.uk.net>
2021-06-15 10:44:55 +0100

    Picking up conversion to OOP using PDO

---
+++ objects/Tracking.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Tracking extends Sesh
+{
+    // database connection and table name
+    private $conn;
+
+    // object properties
+    public $users_id;
+    public $topic, $level, $descriptor;
+    public $KS3_1, $KS3_142;
+
+    
+}
